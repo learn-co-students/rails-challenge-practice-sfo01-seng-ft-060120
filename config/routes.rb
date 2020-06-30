e77df50299015b0c06949d9327bf720fa2259586
sfespinosa <scottjames.espinosa@gmail.com>
2020-06-29 20:54:22 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  resources :buildings, only: [:show, :index, :edit, :update]
+  resources :companies, only: [:new, :create, :show, :edit, :update]
+  resources :employees, only: [:create, :destroy]
+
+
+
 end
